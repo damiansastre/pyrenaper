05f4eb7179608d3e5d2ca00a4c197ebcf7784d44
Damian Sastre <tagerr@gmail.com>
2021-06-24 19:10:31 +1200

    fixing setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyrenaper",
-    version="0.0.7",
+    version="0.0.8",
     author="Damian Sastre",
     author_email="author@example.com",
     description="Python implementation of Argentina's RENAPER API service.",
